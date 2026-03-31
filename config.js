c43048d202047496e19fda22a468dccf8ddd98e2
Tod <tpoirier@nmu.edu>
2026-03-31 13:38:33 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 window.WNMU_UNDERWRITER_CONFIG = {
-  enableSupabase: false,
+  enableSupabase: true,
   supabaseUrl: 'https://wntakzfoprthwggkidyq.supabase.co',
   supabaseAnonKey: 'sb_publishable_gWu_EQ1J3s0iNjeDeINJwQ_xKy8QgAJ',
   workspaceKey: 'wnmu-underwriters',
   cloudTable: 'underwriter_contracts'
+  cloudRunsTable: 'wnmu_underwriter_credit_runs'
+cloudSchema: 'wnmu_underwriters'
 };
